a435548c8b7d7e11ca5c3ab1a4e33e19e94ebfec
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-10 15:50:33 -0500

    added code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    {
+      if (l.includes(word)) return true;
     }
-}
+    //logic for inner loop for vertical word here
+    let verticalLetters = transpose(letters);
+    const verticalJoin = verticalLetters.map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+    
+  }
 
-module.exports = wordSearch
+  return false;
+};
+
+const transpose = function(matrix) {
+  let result = [];//will be our array of arrays
+  for (let rows = 0; rows < matrix[0].length; rows++) {
+    let emptyRow = [];
+    for (let columns = 0; columns < matrix.length; columns++) {
+      emptyRow.push(matrix[columns][rows]);
+    }
+    result.push(emptyRow);
+  }
+  return result;
+};
+  
+module.exports = wordSearch;
